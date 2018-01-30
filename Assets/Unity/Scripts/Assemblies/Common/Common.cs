0ee711f87600778b40245486fcbb5369d7b0bb72
KonH <konh@yandex.ru>
2018-01-30 23:05:07 +0700

    Assembly def test;

---
+++ Assets/Unity/Scripts/Assemblies/Common/Common.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Common : MonoBehaviour {
+	// OK, without link to assembly isn't known
+	// Specific _specific;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
