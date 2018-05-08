d0df719ca8317d67b6ad4ab27c949a011c9283f5
konh <konh@yandex.ru>
2018-05-08 23:37:03 +0700

    Simplify name format;

--- Assets/CSharp/Editor/ListBenchmarks.cs
+++ Assets/CSharp/Editor/ListBenchmarks.cs
@@ -6,14 +6,15 @@ public class ListBenchmarks {
 	[Test]
 	public void Add_Benchmark() {
 		TestAdd<List<int>>(
+			"AddToList",
 			50 * 100,
 			500 * 100,
 			50 * 100 * 100)
 			.WriteToConsole();
 	}
 
-	List<BenchmarkSummary> TestAdd<T>(params int[] iterations) where T : IList<int>, new() {
-		return BenchmarkSummary.TestWrapper<T>("TestAdd", 10, (list, curIters) => {
+	List<BenchmarkSummary> TestAdd<T>(string baseName, params int[] iterations) where T : IList<int>, new() {
+		return BenchmarkSummary.TestWrapper<T>(baseName, 10, (list, curIters) => {
 			for ( var i = 0; i < curIters; i++ ) {
 				list.Add(i);
 			}
