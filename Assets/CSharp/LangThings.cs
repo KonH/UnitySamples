c13f14f1450bd532a1e8c5cbdc62c93142e7d6b1
konh <konh@yandex.ru>
2018-06-29 20:23:01 +0700

    Partial class case added;

---
+++ Assets/CSharp/LangThings.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class LangThings : MonoBehaviour {
+
+	// Partial classes case:
+	// - Class can contains method, which isn't implemented
+	// - It can be private
+	// - It can't return value
+	// - If other part of class contains implementation, it's called
+	// - In other case, it will be ignored
+	
+	sealed partial class SplitClass {
+		public void PublicMethod() {
+			Debug.Log("PublicMethod called");
+			PartialMethod();
+		}
+
+		partial void PartialMethod();
+	}
+
+	
+	sealed partial class SplitClass {
+		partial void PartialMethod() {
+			Debug.Log("PartialMethod called");
+		}
+	}
+
+	void Start() {
+		var a = new SplitClass();
+		a.PublicMethod();
+	}
+}
