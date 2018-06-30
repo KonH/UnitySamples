ca896a55eb7f38f5406c5392df469e2573fb53cf
konh <konh@yandex.ru>
2018-06-30 16:17:55 +0700

    Nums test added;

--- Assets/CSharp/LangTests.cs
+++ Assets/CSharp/LangTests.cs
@@ -161,15 +161,44 @@ public class LangTests : MonoBehaviour {
 	void CastBoxCase() {
 		byte x = 10;
 		int y1 = x; // it is okay, implicit cast
+		Debug.Log(y1);
 
 		object x2 = x;
 		// int y2 = (int)x2; // InvalidCast exception raised, it isn't actual cast, it is unboxing to given type and it allowed only to specific type 
+		Debug.Log(x2);
 
 		int y3 = (byte)x2; // its is okay, correct unboxing and implicit cast
+		Debug.Log(y3);
+	}
+
+	// Num representation case
+
+	void WriteBits(int x) {
+		Debug.LogFormat("{0} = {1}", x, System.Convert.ToString(x, 2).PadLeft(32, '0'));
+	}
+
+	void NumsTest() {
+		Debug.Log(sizeof(int)); // 4
+
+		WriteBits(int.MinValue); // 10000000_00000000_00000000_00000000
+		WriteBits(0           ); // 00000000_00000000_00000000_00000000
+		WriteBits(int.MaxValue); // 01111111_11111111_11111111_11111111
+
+		var x = 10;
+		WriteBits(x);            // 00000000_00000000_00000000_00001010
+
+		var y = -10;
+		WriteBits(y);            // 11111111_11111111_11111111_11110110
+
+		var y2 = ~x + 1;
+		WriteBits(y2);           // 11111111_11111111_11111111_11110110
+
+		var y3 = ~y2 + 1;
+		WriteBits(y3);           // 00000000_00000000_00000000_00001010
 	}
 
 
 	void Update() {
-		CastBoxCase();
+		NumsTest();
 	}
 }
