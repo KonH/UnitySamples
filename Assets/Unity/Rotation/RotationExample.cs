6ff0c2b102657bcc50e2cceec8a2678c7eb1fd90
KonH <konh@yandex.ru>
2018-11-13 20:42:24 +0700

    Initial rotation sample;

---
+++ Assets/Unity/Rotation/RotationExample.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class RotationExample : MonoBehaviour {
+	public float Speed = 10.0f;
+	public Transform Target;
+	public UnityEvent StartCallback;
+	public UnityEvent UpdateCallback;
+
+	void Start() {
+		StartCallback.Invoke();
+	}
+	
+	void Update () {
+		UpdateCallback.Invoke();
+	}
+
+	public void RotateAroundY_EA() {
+		transform.localEulerAngles += Vector3.up * Speed * Time.deltaTime;
+	}
+
+	public void RotateAroundY_Q() {
+		transform.localRotation *= Quaternion.AngleAxis(Speed * Time.deltaTime, Vector3.up);
+	}
+}
