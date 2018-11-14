8d77134fd2a86e80fd0627a23287b357cb925daf
KonH <konh@yandex.ru>
2018-11-14 18:49:39 +0700

    NotNull struct wrapper sample;

---
+++ Assets/CSharp/NotNull.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine.Assertions;
+
+public struct NotNull<T> where T : class {
+	public T Value {
+		get {
+			Assert.IsNotNull(_value);
+			return _value;
+		}
+	}
+
+	readonly T _value;
+	
+	public NotNull(T value) {
+		Assert.IsNotNull(value);
+		_value = value;
+	}
+	
+	public static implicit operator T(NotNull<T> wrapper) {
+		return wrapper.Value;
+	}
+}
+
+public static class NotNullHelper {
+	public static NotNull<T> AsNotNull<T>(this T instance) where T : class {
+		return new NotNull<T>(instance);
+	}
+}
---
+++ Assets/CSharp/NotNullTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class NotNullTests : MonoBehaviour {
+
+	class SomeClass {
+		public string Str { get; }
+
+		public SomeClass(string value) {
+			Str = value;
+		}
+	}
+	
+	void Start () {
+		// Without exceptions it doesn't make sense
+		Assert.raiseExceptions = true;
+		
+		// Works as usual
+		var validObj1 = new NotNull<SomeClass>(new SomeClass("str"));
+		SomeMethod(validObj1);
+		
+		// Alternative syntax
+		var validObj2 = new SomeClass("str").AsNotNull();
+		SomeMethod(validObj2);
+		
+		// In this case we get exception when creating instance
+		try {
+			NotNull<SomeClass> invalidObj = new NotNull<SomeClass>(null);
+			SomeMethod(invalidObj);
+		} catch ( Exception e ) {
+			Debug.LogError(e);
+		}
+
+		// In this case we get exception when getting value
+		try {
+			NotNull<SomeClass> invalidObj = default(NotNull<SomeClass>);
+			SomeMethod(invalidObj);
+		} catch ( Exception e ) {
+			Debug.LogError(e);
+		}
+	}
+
+	void SomeMethod(NotNull<SomeClass> instanceWrapper) {
+		SomeMethodInternal(instanceWrapper);
+	}
+
+	void SomeMethodInternal(SomeClass instance) {
+		Debug.LogFormat("SomeMethod called with '{0}'", (instance != null) ? instance.Str : "null");
+	}
+}
