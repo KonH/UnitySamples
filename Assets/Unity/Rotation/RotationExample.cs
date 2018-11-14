8f8deb4482b2d259ed502d9b0fb37c8c96888530
KonH <konh@yandex.ru>
2018-11-14 19:23:37 +0700

    RotateTo example;

--- Assets/Unity/Rotation/RotationExample.cs
+++ Assets/Unity/Rotation/RotationExample.cs
@@ -1,17 +1,18 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.Events;
 
 public class RotationExample : MonoBehaviour {
-	public float Speed = 10.0f;
-	public Transform Target;
+	public float      Speed = 10.0f;
+	public Transform  Target;
 	public UnityEvent StartCallback;
 	public UnityEvent UpdateCallback;
 
 	void Start() {
 		StartCallback.Invoke();
 	}
-	
-	void Update () {
+
+	void Update() {
 		UpdateCallback.Invoke();
 	}
 
@@ -21,5 +22,19 @@ public class RotationExample : MonoBehaviour {
 
 	public void RotateAroundY_Q() {
 		transform.localRotation *= Quaternion.AngleAxis(Speed * Time.deltaTime, Vector3.up);
+	}
+
+	public void RotateToTarget() {
+		StartCoroutine(RotateToTargetCoro());
+	}
+
+	IEnumerator RotateToTargetCoro() {
+		var targetRotation = Quaternion.LookRotation(Target.position - transform.position);
+		var delta = 0.0f;
+		while ( delta < 1.0f ) {
+			transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, delta * Speed * Time.deltaTime);
+			delta += Time.deltaTime;
+			yield return null;
+		}
 	}
 }
